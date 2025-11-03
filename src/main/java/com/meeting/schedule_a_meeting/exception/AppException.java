cbfe54f5b4109c68ec77992bec18ffb0a38074f5
DangTrongLuong <dangtrongluong4@gmail.com>
2025-11-03 16:11:14 +0700

    Them file userService xu ly CRUD

---
+++ src/main/java/com/meeting/schedule_a_meeting/exception/AppException.java
@@ -0,0 +1,22 @@
+package com.meeting.schedule_a_meeting.exception;
+
+import com.meeting.schedule_a_meeting.enums.ErrorStatus;
+import lombok.Getter;
+
+@Getter
+public class AppException extends RuntimeException {
+    private final ErrorStatus errorStatus;
+    private final String customMessage;
+
+    public AppException(ErrorStatus errorStatus) {
+        super(errorStatus.getMessage());
+        this.errorStatus = errorStatus;
+        this.customMessage = errorStatus.getMessage();
+    }
+
+    public AppException(ErrorStatus errorStatus, String customMessage) {
+        super(customMessage);
+        this.errorStatus = errorStatus;
+        this.customMessage = customMessage;
+    }
+}
