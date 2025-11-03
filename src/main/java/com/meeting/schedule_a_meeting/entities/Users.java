78e19f0b7077fcfdc1236076dc654cc09ef2a9cc
Minh. Vu - CMC Global TTS <vminh@cmcglobal.vn>
2025-11-03 14:12:10 +0700

    tao entity user

--- src/main/java/com/meeting/schedule_a_meeting/entities/Users.java
+++ src/main/java/com/meeting/schedule_a_meeting/entities/Users.java
@@ -1,4 +1,25 @@
 package com.meeting.schedule_a_meeting.entities;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
 public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String displayName;
+    private String avatarUrl;
+
+    @Column(nullable = false)
+    private Boolean isAdmin = false;
 }
