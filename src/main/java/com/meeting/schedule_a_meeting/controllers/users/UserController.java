caa4e7183a1a1f582ecff13fd16b631b59b322b4
Minh. Vu - CMC Global TTS <vminh@cmcglobal.vn>
2025-11-03 15:46:41 +0700

    controller user

--- src/main/java/com/meeting/schedule_a_meeting/controllers/users/UserController.java
+++ src/main/java/com/meeting/schedule_a_meeting/controllers/users/UserController.java
@@ -1,4 +1,20 @@
 package com.meeting.schedule_a_meeting.controllers.users;
 
+import com.meeting.schedule_a_meeting.entities.Users;
+import com.meeting.schedule_a_meeting.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public Users register(@RequestBody Users user) {
+        return userService.register(user);
+    }
 }
