b480f8b8fab3e6119780acc5e9bdcf2f3ea72daa
BAITAP BUOI_02 <dangtrongluong4@gmail.com>
2025-11-07 00:10:50 +0700

    Them ma hoa mk cho admin

--- src/main/java/com/meeting/schedule_a_meeting/ScheduleAMeetingApplication.java
+++ src/main/java/com/meeting/schedule_a_meeting/ScheduleAMeetingApplication.java
@@ -2,12 +2,16 @@ package com.meeting.schedule_a_meeting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class ScheduleAMeetingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScheduleAMeetingApplication.class, args);
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		String password = "Admin@123";
+		System.out.println(encoder.encode(password));
 	}
 
 }
