4cc24fb12765a8ad13fc0319005a592600f4c900
Minh. Vu - CMC Global TTS <vminh@cmcglobal.vn>
2025-11-10 14:34:15 +0700

    fix done

--- src/main/java/com/meeting/schedule_a_meeting/repositories/AdminRepository.java
+++ src/main/java/com/meeting/schedule_a_meeting/repositories/AdminRepository.java
@@ -2,10 +2,12 @@ package com.meeting.schedule_a_meeting.repositories;
 
 import com.meeting.schedule_a_meeting.entities.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface AdminRepository extends JpaRepository<Users, UUID> {
     Optional<Users> findByEmail(String email);
 }
