e5d85177cb8eddc80ac8b43c5bb951a2da9fd964
Lilidelion <nguyenminh569954@gmail.com>
2025-11-11 15:30:20 +0700

    Update duong dan vao file SecurityConfig

--- src/main/java/com/meeting/schedule_a_meeting/sercutity/SecurityConfig.java
+++ src/main/java/com/meeting/schedule_a_meeting/sercutity/SecurityConfig.java
@@ -42,6 +42,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/admin/**").permitAll()
                         .requestMatchers("/api/admin/rooms/**").permitAll()
                         .requestMatchers("/api/admin/devices/**").permitAll()
+                        .requestMatchers("/api/users/meetings/**").permitAll()
 
                         .anyRequest().authenticated())
                 .oauth2Login(oauth2 -> oauth2
