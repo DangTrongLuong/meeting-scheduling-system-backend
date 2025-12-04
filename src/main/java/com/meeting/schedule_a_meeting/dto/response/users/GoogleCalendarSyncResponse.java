2993f0805f7f98773c220c8f554c8c35d3066adf
yrcoksuk <quangdoleminh@gmail.com>
2025-12-04 16:58:54 +0700

    Them file GgCalendarConfig

---
+++ src/main/java/com/meeting/schedule_a_meeting/dto/response/users/GoogleCalendarSyncResponse.java
@@ -0,0 +1,4 @@
+package com.meeting.schedule_a_meeting.dto.response.users;
+
+public class GoogleCalendarSyncResponse {
+}
---
+++ src/main/java/com/meeting/schedule_a_meeting/repositories/GoogleCalendarConnectionRepository.java
@@ -0,0 +1,4 @@
+package com.meeting.schedule_a_meeting.repositories;
+
+public interface GoogleCalendarConnectionRepository {
+}
