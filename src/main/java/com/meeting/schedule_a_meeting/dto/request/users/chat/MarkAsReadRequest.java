4b3590829e3010a69ed1b0f3e37df719ff8525b3
BNgoc005 <nguyenngoc081005@gmail.com>
2025-12-16 16:28:43 +0700

    Them file Dto Request cho Chat va Call

--- src/main/java/com/meeting/schedule_a_meeting/dto/request/users/chat/MarkAsReadRequest.java
+++ src/main/java/com/meeting/schedule_a_meeting/dto/request/users/chat/MarkAsReadRequest.java
@@ -1,4 +1,14 @@
 package com.meeting.schedule_a_meeting.dto.request.users.chat;
 
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class MarkAsReadRequest {
+    private UUID messageId;
 }
