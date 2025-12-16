1acd48ee446af0c88f082096a976e7dc14235524
BNgoc005 <nguyenngoc081005@gmail.com>
2025-12-16 16:45:27 +0700

    Them cac Enum cho Chat va Call

--- src/main/java/com/meeting/schedule_a_meeting/enums/CallAction.java
+++ src/main/java/com/meeting/schedule_a_meeting/enums/CallAction.java
@@ -1,4 +1,11 @@
 package com.meeting.schedule_a_meeting.enums;
 
 public enum CallAction {
+    ACCEPT,
+    DECLINE,
+    END_CALL,
+    MUTE,
+    UNMUTE,
+    ENABLE_VIDEO,
+    DISABLE_VIDEO
 }
--- src/main/java/com/meeting/schedule_a_meeting/enums/UserStatusEnum.java
+++ src/main/java/com/meeting/schedule_a_meeting/enums/UserStatusEnum.java
@@ -1,6 +1,6 @@
 package com.meeting.schedule_a_meeting.enums;
 
-public enum UserStatus {
+public enum UserStatusEnum {
     ONLINE,
     OFFLINE,
     AWAY,
